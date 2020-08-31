39c6430266fee5236eab8ccbad951b6eb035a8e1
mr-n30 <69602501+mr-n30@users.noreply.github.com>
2020-08-31 13:52:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
 # Update the system
-apt update \
-&& apt full-upgrade -y \
-&& apt autoremove -y \
-&& apt install -y nmap masscan make firefox python3-pip python-pip python chromium-browser ssmtp jq \
-&& sleep 5
+sudo apt update \
+&& sudo apt full-upgrade -y \
+&& sudo apt autoremove -y \
+&& sudo apt install -y nmap masscan make firefox python3-pip python-pip python chromium-browser ssmtp jq whois \
+&& sleep 5;
 
 ############
 # INSTALLS #
 ############
 
 # cd into install directory
-cd /opt
+cd /opt;
 
 # Clone sublist3r
 git clone https://github.com/aboul3la/Sublist3r.git \
 && cd Sublist3r/ \
 && pip install -r requirements.txt \
 && ln -sf /opt/Sublist3r/sublist3r.py /usr/bin/sublist3r \
-&& sleep 1
+&& sleep 1;
 
 # Go back to install directory
-cd /opt
+cd /opt;
 
 # Clone geturls
-git clone https://github.com/mr-n30/geturls \
-&& ln -sf /opt/geturls/geturls.py /usr/bin/geturls \
-&& sleep 1
+git clone https://github.com/mr-n30/geturls.git \
+&& cd geturls/ \
+&& pip install -r requirementst.txt \
+&& sleep 1;
 
 # Go back to install directory
-cd /opt
+cd /opt;
 
 # Install massdns
 git clone https://github.com/blechschmidt/massdns.git \
 && cd massdns \
 && make \
 && ln -sf /opt/massdns/bin/massdns /usr/bin/massdns \
-&& sleep 1
+&& sleep 1;
 
 # Install amass
 snap install amass \
 && sudo snap refresh \
-&& sleep 1
+&& sleep 1;
 
 # Install altdns
 pip install py-altdns \
-&& sleep 1
+&& sleep 1;
 
 # Install ffuf
 snap install --classic --channel=1.11/stable go
@@ -54,8 +55,8 @@ go get github.com/ffuf/ffuf \
 && mv ~/go/ ~/ffuf/ \
 && mv ~/ffuf/ /opt/ \
 && ln -sf /opt/ffuf/bin/ffuf /usr/bin/ffuf \
-&& sleep 1
+&& sleep 1;
 
 # Finished
-echo "[+] Tools installed into: /opt"
-echo "[+] Done."
+echo "[+] Tools installed into: /opt";
+echo "[+] Done.";
